bf9b85f8959f86d4d07451664eaaa531c3e41e45
Christoph Badura <bad@bsd.de>
2025-06-04 16:27:40 +0200

    Add add initial state of files from Summer of 2024

---
+++ deploy.py
@@ -0,0 +1,50 @@
+# -*- python -*-
+# manage standard config files on nbvm
+
+import os, platform
+import functools
+
+# from pyinfra import config
+from pyinfra import host
+from pyinfra.operations import files, server
+
+@functools.cache                # probably over-optimized but more correct
+                                # as we're interested in the value "at
+                                # startup."
+def local_hostname():
+    """return the hostname of the machine running pyinfra"""
+    # allow the hostname to be overridden.
+    # strip any attached domain suffix
+    # c.f. https://stackoverflow.com/questions/4271740#comment73605463_4271873
+    os.getenv('HOSTNAME',os.getenv('COMPUTERNAME',
+                                    platform.node())).split('.')[0]
+
+if 'asroot' in host.groups:
+    f = "/etc/shrc"
+    files.put(
+        name=f'Ensure {f}',
+        dest=f,
+        src='files/etc.shrc',
+    )
+
+    f = "/etc/sysctl.conf"
+    sysctl_cnf = files.line(
+        name=f'Ensure ddb.onpanic=1 in {f}',
+        path=f,
+        line=r"^ddb.onpanic.*",
+        replace="ddb.onpanic?=1",
+        ensure_newline=True,
+    )
+    server.shell(
+        name='Apply /etc/sysctl.conf',
+        commands="/etc/rc.d/sysctl start",
+        _if=sysctl_cnf.did_change,
+    )
+
+if 'asuser' in host.groups:
+    for f in ['exrc', 'gdbinit', 'shrc',]:
+        files.put(
+            name=f'Ensure ~/.{f}',
+            dest=f'.{f}',
+            src=f'files/{f}',
+        )
---
+++ inventory.py
@@ -0,0 +1,9 @@
+# pyinfra inventory file
+
+asroot = [
+    ("nbvm-root", {"ssh_hostname": "nbvm", "ssh_user": "root"}),
+]
+
+asuser = [
+        "nbvm",
+]
